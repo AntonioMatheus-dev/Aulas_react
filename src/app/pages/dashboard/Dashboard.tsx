f393371fef63558b14adf74e37df4e9123729b6e
AntonioMatheus-dev <matheus69969@gmail.com>
2025-07-31 10:38:09 -0300

    Formas-de-navegar-entre-Componentes

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,12 @@
+import {Link} from "react-router-dom";
+
 export const Dashboard = () =>{
-return(
+return (
+  <div>
     <p>Pagina de Dashboard</p>
-)
+    <Link to="/entrar">Login</Link>
+  </div>
+);
 }
+
+ 
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Navigate, Route,Routes as Switch } from "react-router-dom";
-import { Dashboard } from "../pages";
+import { BrowserRouter, Navigate, Route, Routes as Switch } from "react-router-dom";
+import { Dashboard,Login} from "../pages";
 
 export const Router = () =>{
 
-    return(
-        <BrowserRouter>
+    return (
+      <BrowserRouter>
         <Switch>
-            <Route path="/Pagina-inicial" element={<Dashboard/>} />
-            <Route path="*" element= {<Navigate to={"/pagina-inicial"}/>}/>
+          <Route path="/Pagina-inicial" element={<Dashboard />} />
+          <Route path="/entrar" element={<Login />} />
+          <Route path="*" element={<Navigate to={"/pagina-inicial"} />} />
         </Switch>
-            {/* Your routes will go here */}
-        </BrowserRouter>
-    )
+      </BrowserRouter>
+    );
 }
