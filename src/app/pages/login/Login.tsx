4635234fb04872b68a534b114fbcfa96af1047de
AntonioMatheus-dev <matheus69969@gmail.com>
2025-07-31 14:16:21 -0300

    feat: add login and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,15 +1,35 @@
-import { useNavigate} from "react-router-dom";
+import { useState } from "react";
 
-export const Login = () =>{
-    const navigate = useNavigate();
-const hendleClick = () =>{
-navigate("/pagina-inicial");
-}
-    return(
-        <div>
-            Login
-            <button onClick={hendleClick}> pagina Inicial</button>
-        </div>
-    );
+export const Login = () => {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const headleEntrar = () => {
 
-}
+    console.log(email);
+    console.log(password);
+
+ }
+
+  return (
+    <div>
+      <form action="">
+
+        <label>
+          <span>Email</span>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+
+        <label>
+          <span>Senha</span>
+          <input
+            type="password" value={password}onChange={(e) => setPassword(e.target.value)}/>
+        </label>
+
+        <button type="button" onClick={headleEntrar}>
+          Entrar
+        </button>
+
+      </form>
+    </div>
+  );
+};
